fa8303eda0f43783807c4c3fc7d2adea0df8ce08
Your Name <vrafiun@gmail.com>
2021-10-01 20:17:06 +0600

    new push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { BrowserRouter as Router,Switch ,Route} from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import OrderReview from './components/OrderReview/OrderReview';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Shop></Shop>
+          </Route>
+          <Route  path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route  path ="/orderReview">
+              <OrderReview></OrderReview>
+          </Route>
+          <Route path ="/inventory">
+            <Inventory></Inventory>
+            </Route>
+            <Route path="*">
+              <NotFound></NotFound>
+            </Route>
+        </Switch>
+      </Router>
+      {/* <Shop></Shop> */}
     </div>
   );
 }
